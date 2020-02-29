c967a869ecbed9eb44996f50ebbcb41e8612e494
Bárbara <babyr.oliveira@gmail.com>
2020-02-28 22:15:45 -0300

    Alterei a frase de acordo com o exercício

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidades do litoral sul da cidade de São Paulo");
+            Console.ReadKey();
+            Console.WriteLine("Bertioga");
+            Console.ReadKey();
+            Console.WriteLine("Ilha Comprida");
+            Console.ReadKey();
+            Console.WriteLine("Peruibe");
+            Console.ReadKey();
+            Console.WriteLine("São Vicente");
+            Console.ReadKey();
+            Console.WriteLine("Caraguatatuba");
+            Console.ReadKey();
+            Console.WriteLine("Ilhabela");
+            Console.ReadKey();
+            Console.WriteLine("Praia Grande");
+            Console.ReadKey();
+            Console.WriteLine("Ubatuba");
+            Console.ReadKey();
+            Console.WriteLine("Cubatão");
+            Console.ReadKey();
+            Console.WriteLine("Itanhaém");
+            Console.ReadKey();
+            Console.WriteLine("Santos");
+            Console.ReadKey();
+            Console.WriteLine("Guarujá");
+            Console.ReadKey();
+            Console.WriteLine("Mongaguá");
+            Console.ReadKey();
+            Console.WriteLine("São Sebastião");
+            
+        }
+    }
+}
